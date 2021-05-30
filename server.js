fd233801573deaa50233adf6a993955f890be0c3
Nils Trautner <nils.trautner@web.de>
2021-05-30 21:56:51 +0200

    server.js change

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/darts-project'));
+app.use(express.static('./dist/'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/darts-project/'}),
+app.get('/*', (_, res) =>
+    res.sendFile('index.html', {root: 'dist/'}),
 );
 
 // Start the app by listening on the default Heroku port
