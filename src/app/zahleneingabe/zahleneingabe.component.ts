cab2e658899f4b86b37249ab152bb127fdde6734
Nils Trautner <nils.trautner@web.de>
2021-06-01 17:26:18 +0200

    New sound files

--- src/app/zahleneingabe/zahleneingabe.component.ts
+++ src/app/zahleneingabe/zahleneingabe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-zahleneingabe',
   templateUrl: './zahleneingabe.component.html',
@@ -8,16 +9,17 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class ZahleneingabeComponent implements OnInit {
-
+  //Allgemeine Einstellungen
   summe = "0";
   startPunkte = 501;
   maxLegs = 3;
   maxSets = 3;
   textUnten = "2021 World Championship - Round 1";
+  public button_symbole = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "0", "b"];
 
+  //Spieler Eigenschaften und Einstellungen
   s1_name = "Spieler 1";
   s2_name = "Spieler 2";
-
   s1_punkte = this.startPunkte;
   s2_punkte = this.startPunkte;
   s1_sets = 0;
@@ -29,18 +31,15 @@ export class ZahleneingabeComponent implements OnInit {
   s1_finishway = "-1";
   s2_finishway = "-1";
 
-  turn_s1 = true;
+  turn_s1 = true;           //Wer ist dran, SP1? 
 
   //Sound
-  soundStatus = false;
-  soundStatusCl = true;
-  bgaudio = new Audio();
+  soundStatus = false;      //Background Music Status
+  soundStatusCl = true;     //Callout Music Status
+  bgaudio = new Audio();    
   claudio = new Audio();
 
-  testaudio = new Audio();
-
-  public test = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "0", "b"];
-  //public finish = ["T20 T20 Bull", "Kein Finish", "Kein Finish", "T20 T19 Bull", "Kein Finish", "Kein Finish", "T20 T18 Bull", "Kein Finish", "Kein Finish", "T20 T17 Bull", "T20 T20 D20", "Kein Finish", "T20 T20 D19", "T20 T19 D20", "T20 T20 D18", "T20 T19 D19", "T20 T18 D20", "T20 T19 D18", "T20 T20 D16", "T20 T17 D20", "T20 T18 D18", "T20 T19 D16", "T20 T16 D20", "T19 T18 D18", "T20 T18 D16", "T20 T15 D20", "T18 T18 D18", "T20 T17 D16", "T20 T14 D20", "T17 T18 D18", "T20 T20 D10", "T19 T14 D20", "T20 T18 D12", "T19 T20 D10", "T20 T20 D8", "T17 T20 D12", "T20 T14 D16", "T20 T11 D20", "T20 T12 D18", "T20 T17 D10", "T20 S20 Bull", "T19 T12 D18", "T18 T14 D16", "T20 T17 D8", "T19 S19 Bull", "25 T20 D20", "T20 S14 Bull", "T19 T10 D18", "T18 T12 D16", "T20 T7 D20", "T20 S20 D20", "T20 S19 D20", "T20 S18 D20", "T20 S17 D20", "T20 S16 D20", "T20 S15 D20", "T20 S14 D20", "T20 S13 D20", "T20 S20 D16", "T20 S19 D16", "T20 S18 D16", "T20 S17 D16", "T20 S16 D16", "T19 Bull", "T20 S10 D18", "T20 S13 D16", "T18 Bull", "T20 S3 D20", "T20 S10 D16", "T17 Bull", "T20 D20", "T20 S7 D16", "T20 D19", "T19 D20", "T20 D18", "T19 D19", "T18 D20", "T19 D18", "T20 D16", "T17 D20", "T18 D18", "T19 D16", "T16 D20", "T17 D18", "T18 D16", "T15 D20", "T20 D12", "T17 D16", "T14 D20", "T15 D18", "T20 D10", "T13 D20", "T18 D12", "T19 D10", "T20 D8", "T17 D12", "T18 D10", "T19 D8", "T12 D18", "T17 D10", "T10 D20", "T13 D15", "T20 D4", "T17 D8", "T10 D18", "25 D20", "T16 D8", "T13 D12", "T10 D16", "25 D18", "S20 D20", "S19 D20", "S18 D20", "S17 D20", "S16 D20", "S15 D20", "S14 D20", "S13 D20", "S20 D16", "S19 D16", "S18 D16", "S17 D16", "S16 D16", "S15 D16", "S6D 20", "S13 D16", "S12 D16", "S11 D16", "S10 D16", "S9 D16", "D20", "S7 D16", "D19", "S5 D16", "D18", "S3 D16", "D17", "S17 D8", "D16", "S15 D8", "D15", "S13 D8", "D14", "S11 D8", "D13", "S9 D8", "D12", "S7 D8", "D11", "S5 D8", "D10", "S3 D8", "D9", "S1 D8", "D8", "S7 D4", "D7", "S5 D4", "D6", "S3 D4", "D5", "S1 D4", "D4", "S3 D2", "D3", "S1 D2", "D2", "S1 D2", "D1"];
+  //Finish Ways
   public finish = ["T20 T20 Bull", "-1", "-1", "T20 T19 Bull", "-1", "-1", "T20 T18 Bull", "-1", "-1", "T20 T17 Bull", "T20 T20 D20", "-1", "T20 T20 D19", "T20 T19 D20", "T20 T20 D18", "T20 T19 D19", "T20 T18 D20", "T20 T19 D18", "T20 T20 D16", "T20 T17 D20", "T20 T18 D18", "T20 T19 D16", "T20 T16 D20", "T19 T18 D18", "T20 T18 D16", "T20 T15 D20", "T18 T18 D18", "T20 T17 D16", "T20 T14 D20", "T17 T18 D18", "T20 T20 D10", "T19 T14 D20", "T20 T18 D12", "T19 T20 D10", "T20 T20 D8", "T17 T20 D12", "T20 T14 D16", "T20 T11 D20", "T20 T12 D18", "T20 T17 D10", "T20 20 Bull", "T19 T12 D18", "T18 T14 D16", "T20 T17 D8", "T19 19 Bull", "25 T20 D20", "T20 14 Bull", "T19 T10 D18", "T18 T12 D16", "T20 T7 D20", "T20 20 D20", "T20 19 D20", "T20 18 D20", "T20 17 D20", "T20 16 D20", "T20 15 D20", "T20 14 D20", "T20 13 D20", "T20 20 D16", "T20 19 D16", "T20 18 D16", "T20 17 D16", "T20 16 D16", "T19 Bull", "T20 10 D18", "T20 13 D16", "T18 Bull", "T20 3 D20", "T20 10 D16", "T17 Bull", "T20 D20", "T20 7 D16", "T20 D19", "T19 D20", "T20 D18", "T19 D19", "T18 D20", "T19 D18", "T20 D16", "T17 D20", "T18 D18", "T19 D16", "T16 D20", "T17 D18", "T18 D16", "T15 D20", "T20 D12", "T17 D16", "T14 D20", "T15 D18", "T20 D10", "T13 D20", "T18 D12", "T19 D10", "T20 D8", "T17 D12", "T18 D10", "T19 D8", "T12 D18", "T17 D10", "T10 D20", "T13 D15", "T20 D4", "T17 D8", "T10 D18", "25 D20", "T16 D8", "T13 D12", "T10 D16", "25 D18", "20 D20", "19 D20", "18 D20", "17 D20", "16 D20", "15 D20", "14 D20", "13 D20", "20 D16", "19 D16", "18 D16", "17 D16", "16 D16", "15 D16", "6D 20", "13 D16", "12 D16", "11 D16", "10 D16", "9 D16", "D20", "7 D16", "D19", "5 D16", "D18", "3 D16", "D17", "17 D8", "D16", "15 D8", "D15", "13 D8", "D14", "11 D8", "D13", "9 D8", "D12", "7 D8", "D11", "5 D8", "D10", "3 D8", "D9", "1 D8", "D8", "7 D4", "D7", "5 D4", "D6", "3 D4", "D5", "1 D4", "D4", "3 D2", "D3", "1 D2", "D2", "1 D2", "D1"];
 
   contructor() { 
@@ -48,22 +47,16 @@ export class ZahleneingabeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    //this.bgaudio.src = "../../../assets/bg1.mp3";
-    //this.bgaudio.src ="https://gitlab.com/nilstrautner/darts/-/raw/master/src/assets/bg1.mp3";
-    //this.bgaudio.load();
-  }
 
+  }
 
   changeSoundStatus(status : boolean) : void {
     this.soundStatus = status;
-    
+
     //Backgroudn Sound abspielen
     if (this.soundStatus) {
-      
-
       this.bgaudio.src = "../assets/bg1.mp3"
       this.bgaudio.play();
-      this.claudio.play();
       console.log("Sound gespielt");
     } else {
       this.bgaudio.pause();
@@ -71,27 +64,19 @@ export class ZahleneingabeComponent implements OnInit {
   }
 
   playSound(i : number) : void {
-    console.log("Play sound");
+
     if (this.soundStatusCl) {
-      if (i == 180) {
-        //this.claudio.src = "../../../assets/180.mp3";
-        
+      /* if (i == 180) {
         this.claudio.src = "../assets/180.mp3";
-        //this.claudio.load();
         this.claudio.play();
-      }
-      if (i == 140) {
-        //this.claudio.src = "../../../assets/140.wav";
-        this.claudio.src = "../assets/140.wav";
-        //this.claudio.load();
+      } */
+      try {
+        this.claudio.src = "../assets/" + i + ".wav";
         this.claudio.play();
-        
-      }
-      if (i == 100) {
-        //this.claudio.src = "../../../assets/100.wav";
-        this.claudio.src = "../assets/100.wav";
-        //this.claudio.load();
-        this.claudio.play();
+        console.log("Sound played of number " + i);
+      } catch (error) { //Try Catch funktioniert nicht, bei nicht existierender Datei standart 404 Fehler-Meldung 
+        console.log("Sound of number " + i + " not found");
+        console.log(error);
       }
     }
   }
@@ -197,7 +182,6 @@ export class ZahleneingabeComponent implements OnInit {
   }
   
   klicken(nr : string) : void {
-
     //Submit
     if (nr == "b") {
       this.submit(this.summe);
