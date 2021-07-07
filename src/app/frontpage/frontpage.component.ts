0030c183bd3cb675b6455a964faeacdd3098de9c
Nils Trautner <nils.trautner@web.de>
2021-07-07 21:49:39 +0200

    menu added

--- src/app/frontpage/frontpage.component.ts
+++ src/app/frontpage/frontpage.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-frontpage',
   templateUrl: './frontpage.component.html',
-  styleUrls: ['./frontpage.component.sass']
+  styleUrls: ['./frontpage.component.sass', './frontpage_menu.sass']
 })
 export class FrontpageComponent implements OnInit {
+
+  showing_menu = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.menu_button_click(); //wegmachen für menu
   }
 
+  menu_button_click() : void{
+    document.getElementById("menu_bullets")!.style.display = "none";
+    this.showing_menu = !this.showing_menu;
+
+    if (this.showing_menu) {
+      document.getElementById("menu_bullets")!.style.display = "flex";
+      document.getElementById("inhalt")!.style.display = "none";
+    } else {
+      document.getElementById("menu_bullets")!.style.display = "none";
+      document.getElementById("inhalt")!.style.display = "block";
+    }
+   
+  }
 }
